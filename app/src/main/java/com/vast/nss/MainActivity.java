a35413cf1248ad1d708a2e9875e8dc71d73a622f
HashimJVZ <highbrowdirector@gmail.com>
2019-10-11 01:09:39 +0530

    profile Fragment creation

--- app/src/main/java/com/vast/nss/MainActivity.java
+++ app/src/main/java/com/vast/nss/MainActivity.java
@@ -1,22 +1,71 @@
 package com.vast.nss;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.vast.nss.Fragment.UserProfileFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.logo);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation =  findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
 
     }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()){
+
+            case R.id.navigation_event:
+                fragment = new UserProfileFragment();
+                break;
+
+            case R.id.navigation_attendance:
+                fragment = new UserProfileFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new UserProfileFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
+    }
+
+
 }
