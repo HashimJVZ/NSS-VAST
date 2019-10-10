0395a880ac336626e2f9664583fb7adc68e8844a
HashimJVZ <highbrowdirector@gmail.com>
2019-10-10 23:49:33 +0530

    Initial commit

---
+++ app/src/main/java/com/vast/nss/MainActivity.java
@@ -0,0 +1,22 @@
+package com.vast.nss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setContentView(R.layout.activity_main);
+
+    }
+}
