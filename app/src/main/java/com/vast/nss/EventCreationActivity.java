6be754813082b5e5064110358d3e7563002944f7
HashimJVZ <highbrowdirector@gmail.com>
2019-10-21 01:35:41 +0530

    Added Event Creation Activity Changed layouts
    App crashes on Event Fragment

--- app/src/main/java/com/vast/nss/EventCreationActivity.java
+++ app/src/main/java/com/vast/nss/EventCreationActivity.java
@@ -1,4 +1,35 @@
 package com.vast.nss;
 
-public class EventCreationActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import com.vast.nss.Fragment.DatePickerFragment;
+
+public class EventCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categoty_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
 }
