7262dd3b1e39ac2bbc0b8411d13e2862537476a8
HashimJVZ <highbrowdirector@gmail.com>
2019-11-07 07:24:49 +0530

    User listing

---
+++ app/src/main/java/com/vast/nss/Fragment/UserListFragment.java
@@ -0,0 +1,9 @@
+package com.vast.nss.Fragment;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserListFragment extends Fragment {
+
+    private RecyclerView userListRecyclerView;
+}
