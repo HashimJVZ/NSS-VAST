f0ceb78ff81585a9b0c0471b2a9a806e66f9dae1
HashimJVZ <hashimtvr2@gmail.com>
2019-11-22 23:54:51 +0530

    added few declarations

--- app/src/main/java/com/vast/nss/Fragment/ProfileFragment.java
+++ app/src/main/java/com/vast/nss/Fragment/ProfileFragment.java
@@ -11,14 +11,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.vast.nss.R;
 
 public class ProfileFragment extends Fragment {
 
-    TextView profileName, profileId, profileHours, profileUnit, profileDept, ProfileContact,
+    private TextView profileName, profileId, profileHours, profileUnit, profileDept, profileContact,
         communityHour, campHour, orientationHour, campusHour;
 
-    DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference =  firebaseDatabase.getReference();
 
     @Nullable
     @Override
@@ -27,6 +29,15 @@ public class ProfileFragment extends Fragment {
         profileName = profileName.findViewById(R.id.profile_name);
         profileId = profileId.findViewById(R.id.profile_userid);
         profileHours = profileHours.findViewById(R.id.profile_hours);
+        profileUnit = profileUnit.findViewById(R.id.profile_unit);
+        profileDept = profileDept.findViewById(R.id.profile_dept);
+        profileContact = profileContact.findViewById(R.id.profile_contact);
+        communityHour = communityHour.findViewById(R.id.community_hour);
+        campHour = campHour.findViewById(R.id.camp_hour);
+        orientationHour = orientationHour.findViewById(R.id.orientation_hour);
+        campusHour = campusHour.findViewById(R.id.campus_hour);
+
+
 
         return inflater.inflate(R.layout.fragment_profile1, container, false);
 
