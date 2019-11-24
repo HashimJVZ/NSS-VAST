7a2fdaf458145f156334f97970e8124f2402fe87
HashimJVZ <highbrowdirector@gmail.com>
2019-11-24 08:33:46 +0530

    Scanning button implementation bug exists

--- app/src/main/java/com/vast/nss/Fragment/AttendanceFragment.java
+++ app/src/main/java/com/vast/nss/Fragment/AttendanceFragment.java
@@ -1,21 +1,41 @@
 package com.vast.nss.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.vast.nss.MainActivity;
 import com.vast.nss.R;
+import com.vast.nss.ScanningActivity;
 
 public class AttendanceFragment extends Fragment {
+
+    Button button;
+    Context context;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_attendance, container, false);
+        View view = inflater.inflate(R.layout.fragment_attendance, container, false);
+
+        button= view.findViewById(R.id.scanBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,ScanningActivity.class);
+                context.startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 }
