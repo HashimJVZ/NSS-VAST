d70d8d45cd3aa5f368d20984993a952208ef3e41
HashimJVZ <hashimtvr2@gmail.com>
2019-12-20 12:47:42 +0530

    Initial cleanup and setup on AttendanceFragment

--- app/src/main/java/com/vast/nss/Fragment/AttendanceFragment.java
+++ app/src/main/java/com/vast/nss/Fragment/AttendanceFragment.java
@@ -6,29 +6,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.vast.nss.MainActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.vast.nss.R;
 import com.vast.nss.ScanningActivity;
 
 public class AttendanceFragment extends Fragment {
 
-    Button button;
-    Context context;
+    private Context context;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_attendance, container, false);
 
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.scan_fab);
         context = getContext();
-        button= view.findViewById(R.id.scanBtn);
-        button.setOnClickListener(new View.OnClickListener() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context,ScanningActivity.class);
