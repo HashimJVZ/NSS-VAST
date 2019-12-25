8ad984d48841f9dc2fec60abb50d1d5f65856091
HashimJVZ <hashimtvr2@gmail.com>
2019-12-25 22:55:37 +0530

    Created a splash screen with temporary onclick pass

---
+++ app/src/main/java/com/vast/nss/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.vast.nss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        textView = findViewById(R.id.temp);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
