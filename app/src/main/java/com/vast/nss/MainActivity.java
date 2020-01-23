86a35877a48231f29f118ceb760042b67fe92cbd
HashimJVZ <highbrowdirector@gmail.com>
2020-01-23 13:40:49 +0530

    Fixed Splash reverse issue

--- app/src/main/java/com/vast/nss/MainActivity.java
+++ app/src/main/java/com/vast/nss/MainActivity.java
@@ -21,23 +21,15 @@ import com.vast.nss.Fragment.ProfileFragment;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, EventFragment.ClickListenerEvent {
-    public static int SPLASH_TIME_OUT = 4000;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.P)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent splashintent = new Intent(MainActivity.this, SplashActivity.class);
-                startActivity(splashintent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.logo);
--- app/src/main/java/com/vast/nss/SplashActivity.java
+++ app/src/main/java/com/vast/nss/SplashActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 4000;
 
     TextView textView;
 
@@ -15,6 +17,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashintent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashintent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
 
         textView = findViewById(R.id.temp);
         textView.setOnClickListener(new View.OnClickListener() {
