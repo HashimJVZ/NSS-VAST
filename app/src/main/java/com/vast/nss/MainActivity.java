a6b22f7dde3a7249855931397f7f873a217b3107
HashimJVZ <hashimtvr2@gmail.com>
2020-01-23 08:18:49 +0530

    Made Splash working as splash

--- app/src/main/java/com/vast/nss/MainActivity.java
+++ app/src/main/java/com/vast/nss/MainActivity.java
@@ -1,12 +1,15 @@
 package com.vast.nss;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -15,15 +18,28 @@ import com.vast.nss.Fragment.AttendanceFragment;
 import com.vast.nss.Fragment.EventFragment;
 import com.vast.nss.Fragment.ProfileFragment;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, EventFragment.ClickListenerEvent {
+    public static int SPLASH_TIME_OUT = 4000;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashintent = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(splashintent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.logo);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
