2d1ae807d463aee812097b07a50a02cc1702d502
HashimJVZ <highbrowdirector@gmail.com>
2020-02-01 23:53:21 +0530

    Added attendance activity, need to create adapter, to show students who participates

---
+++ app/src/main/java/com/vast/nss/Adapter/AttendanceAdapter.java
@@ -0,0 +1,10 @@
+//package com.vast.nss.Adapter;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class AttendanceAdapter extends RecyclerView.Adapter<AttendanceAdapter.MyViewHolder> {
+//
+//
+//    public class onCreateMyViewHolder {
+//    }
+//}
