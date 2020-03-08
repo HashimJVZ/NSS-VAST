2a6ef76cf9bbe0706de788612ef55ae8e92fe903
HashimJVZ <hashimtvr2@gmail.com>
2020-03-08 09:48:04 +0530

    clear userName on logOut

--- app/src/main/java/com/vast/nss/MainActivity.java
+++ app/src/main/java/com/vast/nss/MainActivity.java
@@ -1,6 +1,7 @@
 package com.vast.nss;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 //clear userName from shared preferences
+                clearUser();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
@@ -52,6 +54,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         });
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void clearUser() {
+        SharedPreferences sharedPreferences = getSharedPreferences("SharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("userName", null);
+        editor.apply();
     }
 
     private boolean loadFragment(Fragment fragment) {
