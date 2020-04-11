0fd60b33a2bdec8ef704b56f995146e1db1df050
HashimJVZ <hashimtvr2@gmail.com>
2020-04-11 17:55:54 +0530

    layout update loginActivity , code cleanUp and Rearrange

--- app/src/main/java/com/vast/nss/SplashActivity.java
+++ app/src/main/java/com/vast/nss/SplashActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
