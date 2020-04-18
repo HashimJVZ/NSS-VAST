c3458719f7a75249491fd1836f6c8b769c90ec8a
HashimJVZ <hashimtvr2@gmail.com>
2020-04-18 11:29:11 +0530

    AboutActivity load photo from firebase, heading added in layout, slight elevation changes.

--- app/src/main/java/com/vast/nss/AboutActivity.java
+++ app/src/main/java/com/vast/nss/AboutActivity.java
@@ -2,13 +2,45 @@ package com.vast.nss;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class AboutActivity extends AppCompatActivity {
+
+    private CircleImageView developer_pic;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        developer_pic = findViewById(R.id.developerPic);
+
+        databaseReference.child("developer").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String photoUrl = (String) dataSnapshot.child("photo").getValue();
+                Picasso.get().load(photoUrl).into(developer_pic);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
