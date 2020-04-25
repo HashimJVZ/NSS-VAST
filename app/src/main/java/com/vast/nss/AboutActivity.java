14c77328e76cea63752339338ac09020f320e394
HashimJVZ <hashimtvr2@gmail.com>
2020-04-25 14:28:09 +0530

    About Activity got some contents

--- app/src/main/java/com/vast/nss/AboutActivity.java
+++ app/src/main/java/com/vast/nss/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.vast.nss;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +21,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class AboutActivity extends AppCompatActivity {
 
     private CircleImageView developer_pic;
+
+    ImageView instagram, github, twitter;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -39,6 +45,33 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        instagram = findViewById(R.id.imageViewInstagram);
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hashim_jvz/"));
+                startActivity(browserIntent);
+            }
+        });
+
+        github = findViewById(R.id.imageViewGithub);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HashimJVZ"));
+                startActivity(browserIntent);
+            }
+        });
+
+        twitter = findViewById(R.id.imageViewTwitter);
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/hashimjvz"));
+                startActivity(browserIntent);
             }
         });
 
